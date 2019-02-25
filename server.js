54a8207b253affb400573a2e493683ce55f0b29f
raul-f <raulf.dev@gmail.com>
2019-02-25 15:49:34 -0300

    auth error has been fixed

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const GitHubStrategy = require('passport-github')
 const session = require('express-session')
 const dotenv = require('dotenv')
 const path = require('path')
-let userProfile
+
+let userProfile = {}
 
 const app = express()
 dotenv.config()
@@ -29,16 +30,32 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: 'http://localhost:3000/auth/github/callback',
+			callbackURL: 'http://localhost:8080/auth/github/callback',
 		},
 		function(accessToken, refreshToken, profile, cb) {
-			userProfile = profile
+			userProfile.profile = profile
+			userProfile.accessToken = accessToken
+			console.log(userProfile)
 			return cb(null)
 		}
 	)
 )
 
-app.route('/auth/github').get(passport.authenticate('github'))
+app.route('/auth/github/').get(
+	passport.authenticate('github', {
+		scope: [
+			'user',
+			'public_repo',
+			'repo',
+			'repo_deployment',
+			'repo:status',
+			'read:repo_hook',
+			'read:org',
+			'read:public_key',
+			'read:gpg_key',
+		],
+	})
+)
 
 app.route('/auth/github/callback').get(
 	passport.authenticate('github', {
@@ -48,8 +65,8 @@ app.route('/auth/github/callback').get(
 )
 
 app.route('/userdata').get((req, res) => {
-	res.json({ profile: userProfile })
-	userProfile = null
+	res.json({ ...userProfile })
+	userProfile = {}
 })
 
 app.use('/bundle.js', express.static('./dist/bundle.js'))
